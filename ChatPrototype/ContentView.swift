a4177052376fb4fda0d548e22e91bad130dcf013
James Kerrane <hi@jameskerrane.com>
2024-06-03 16:40:28 -0600

    Hello, Explore Xcode!

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -9,11 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 25) {
+            Text("¡Holisssss!")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            Text("¿Qué onda linda?")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            Text("Nada mucho, solo probando Xcode ;))")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            Text("¡Buena suerte!")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            Text("Gracias, ¡adiós!")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            Text("¡Hasta luego!")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
         }
         .padding()
     }
