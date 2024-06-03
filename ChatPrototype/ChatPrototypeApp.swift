d3845e92be33e1d5114d104f17db8fcb7ed9adcb
James Kerrane <hi@jameskerrane.com>
2024-06-03 15:38:11 -0600

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by James Kerrane on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
