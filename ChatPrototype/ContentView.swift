47480c7d773d1deafbc8ca0dca0eaf2861611628
James Kerrane <hi@jameskerrane.com>
2024-06-04 10:52:41 -0600

    Add custom view for ChatBubble

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -10,30 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(spacing: 25) {
-            Text("¡Holisssss!")
-                .padding()
-                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
-                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-            Text("¿Qué onda linda?")
-                .padding()
-                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
-                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-            Text("Nada mucho, solo probando Xcode ;))")
-                .padding()
-                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
-                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-            Text("¡Buena suerte!")
-                .padding()
-                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
-                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-            Text("Gracias, ¡adiós!")
-                .padding()
-                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
-                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-            Text("¡Hasta luego!")
-                .padding()
-                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
-                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            ChatBubble(message: "¡Holisssss!", isSender: true)
+            ChatBubble(message: "¿Qué onda linda?", isSender: false)
+            ChatBubble(message: "Nada mucho, solo probando Xcode ;))", isSender: true)
+            ChatBubble(message: "¡Buena suerte!", isSender: false)
+            ChatBubble(message: "Gracias, ¡adiós!", isSender: true)
+            ChatBubble(message: "¡Hasta luego!", isSender: false)
         }
         .padding()
     }
@@ -41,4 +23,24 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct ChatBubble: View {
+    let message: String
+    let isSender: Bool
+    
+    var bubbleColor: Color {
+        if(isSender) {
+            return Color.yellow
+        } else {
+            return Color.teal
+        }
+    }
+    
+    var body: some View {
+        Text(message)
+            .padding()
+            .background(bubbleColor, in: RoundedRectangle(cornerRadius: 8))
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+    }
 }
